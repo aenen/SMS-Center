5aaf2491786ca3c1f17c3a6c81f550352082a859
aenen <yaroslav.golyuk@yandex.ua>
2016-12-10 22:54:49 +0200

    commit from VS2015

--- Lib SMS/SMS Sender.cs
+++ Lib SMS/SMS Sender.cs
@@ -65,7 +65,7 @@ namespace Lib_SMS
         /// -2 — Неправильный формат XML
         /// -3 — Недостаточно кредитов на аккаунте пользователя
         /// -4 — Нет верных номеров получателей
-        /// -5 — Ошибка подключения/исключение</returns>
+        /// -5 — Ошибка подключения</returns>
         public int Send(string number, string message, string messageId = "")
         {
             try
@@ -99,11 +99,12 @@ namespace Lib_SMS
                 request.ContentLength = data.Length;
                 Stream dataStream = request.GetRequestStream();
                 dataStream.Write(data, 0, data.Length);
+
                 using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
                     if (response.StatusCode != HttpStatusCode.OK)
                     {
-                        Console.WriteLine($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
+                        ShowErrorMessage($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
                         return -5;
                     }
 
@@ -120,7 +121,7 @@ namespace Lib_SMS
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                ShowErrorMessage(e.Message);
                 return -5;
             }
         }
@@ -129,8 +130,8 @@ namespace Lib_SMS
         /// Получение статуса SMS
         /// </summary>
         /// <param name="messageId">Уникальный ключ сообщения</param>
-        /// <returns>Строка с датами отправки и доставки, а также статусом сообщения</returns>
-        public string GetState(string messageId)
+        /// <returns>Статус сообщения</returns>
+        public SMSState GetState(string messageId)
         {
             try
             {
@@ -155,12 +156,13 @@ namespace Lib_SMS
                 request.ContentLength = data.Length;
                 Stream dataStream = request.GetRequestStream();
                 dataStream.Write(data, 0, data.Length);
+
                 using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
                     if (response.StatusCode != HttpStatusCode.OK)
                     {
-                        Console.WriteLine($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
-                        return "Server error";
+                        ShowErrorMessage($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
+                        return SMSState.ERROR;
                     }
 
                     StreamReader reader = new StreamReader(response.GetResponseStream());
@@ -168,24 +170,162 @@ namespace Lib_SMS
 
                     using (XmlReader xmlReader = XmlReader.Create(new StringReader(xmlResult)))
                     {
-                        string status = "";
+                        SMSState status;
                         xmlReader.ReadToFollowing("message");
-                        xmlReader.MoveToAttribute("id");
-                        status += $"——— ID: {xmlReader.Value}\n";
-                        xmlReader.MoveToAttribute("sentdate");
-                        status += $"    Отослано: {xmlReader.Value}\n";
-                        xmlReader.MoveToAttribute("donedate");
-                        status += $"    Доставлено: {xmlReader.Value}\n";
                         xmlReader.MoveToAttribute("status");
-                        status += $"    Статус: {xmlReader.Value}\n";
-                        return status;
+                        try
+                        {
+                            status = (SMSState)Enum.Parse(typeof(SMSState), xmlReader.Value);
+                            return status;
+                        }
+                        catch (Exception)
+                        {
+                            status = SMSState.ERROR;
+                            return status;
+                        }
                     }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                return "Exception occurred";
+                ShowErrorMessage(e.Message);
+                return SMSState.ERROR;
+            }
+        }
+
+        /// <summary>
+        /// Получение даты отправки SMS
+        /// </summary>
+        /// <param name="messageId">Уникальный ключ сообщения</param>
+        /// <returns>not null — Дата отправки сообщения
+        /// null — Ошибка подключения</returns>
+        public DateTime? GetSendDate(string messageId)
+        {
+            try
+            {
+                string XML = "XML=<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                                "<SMS>\n" +
+                                    "<operations>\n" +
+                                        "<operation>GETSTATUS</operation>\n" +
+                                    "</operations>\n" +
+                                    "<authentification>\n" +
+                                        $"<username>{Login}</username>\n" +
+                                        $"<password>{Password}</password>\n" +
+                                    "</authentification>\n" +
+                                    "<statistics>\n" +
+                                        $"<messageid>{messageId}</messageid>\n" +
+                                    "</statistics>\n" +
+                                "</SMS>\n";
+                HttpWebRequest request = WebRequest.Create("http://api.myatompark.com/members/sms/xml.php") as HttpWebRequest;
+                request.Method = "Post";
+                request.ContentType = "application/x-www-form-urlencoded";
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] data = encoding.GetBytes(XML);
+                request.ContentLength = data.Length;
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(data, 0, data.Length);
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        ShowErrorMessage($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
+                        return null;
+                    }
+
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    string xmlResult = reader.ReadToEnd();
+
+                    using (XmlReader xmlReader = XmlReader.Create(new StringReader(xmlResult)))
+                    {
+                        DateTime? sendDate;
+                        xmlReader.ReadToFollowing("message");
+                        xmlReader.MoveToAttribute("sentdate");
+                        try
+                        {
+                            sendDate = DateTime.Parse(xmlReader.Value);
+                            return sendDate;
+                        }
+                        catch (Exception e)
+                        {
+                            ShowErrorMessage(e.Message);
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ShowErrorMessage(e.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Получение даты доставки SMS
+        /// </summary>
+        /// <param name="messageId">Уникальный ключ сообщения</param>
+        /// <returns>not null — Дата доставки сообщения
+        /// null — Ошибка подключения</returns>
+        public DateTime? GetDeliveryDate(string messageId)
+        {
+            try
+            {
+                string XML = "XML=<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                                "<SMS>\n" +
+                                    "<operations>\n" +
+                                        "<operation>GETSTATUS</operation>\n" +
+                                    "</operations>\n" +
+                                    "<authentification>\n" +
+                                        $"<username>{Login}</username>\n" +
+                                        $"<password>{Password}</password>\n" +
+                                    "</authentification>\n" +
+                                    "<statistics>\n" +
+                                        $"<messageid>{messageId}</messageid>\n" +
+                                    "</statistics>\n" +
+                                "</SMS>\n";
+                HttpWebRequest request = WebRequest.Create("http://api.myatompark.com/members/sms/xml.php") as HttpWebRequest;
+                request.Method = "Post";
+                request.ContentType = "application/x-www-form-urlencoded";
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] data = encoding.GetBytes(XML);
+                request.ContentLength = data.Length;
+                Stream dataStream = request.GetRequestStream();
+                dataStream.Write(data, 0, data.Length);
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        ShowErrorMessage($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
+                        return null;
+                    }
+
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    string xmlResult = reader.ReadToEnd();
+
+                    using (XmlReader xmlReader = XmlReader.Create(new StringReader(xmlResult)))
+                    {
+                        DateTime? deliveryDate;
+                        xmlReader.ReadToFollowing("message");
+                        xmlReader.MoveToAttribute("donedate");
+                        try
+                        {
+                            deliveryDate = DateTime.Parse(xmlReader.Value);
+                            return deliveryDate;
+                        }
+                        catch (Exception e)
+                        {
+                            ShowErrorMessage(e.Message);
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ShowErrorMessage(e.Message);
+                return null;
             }
         }
 
@@ -197,7 +337,7 @@ namespace Lib_SMS
         /// <returns>>0 — Цена отправки сообщения в гривнах
         /// -1 — Неправильный логин и/или пароль
         /// -2 — Неправильный формат XML
-        /// -3 — Ошибка подключения/исключение</returns>
+        /// -3 — Ошибка подключения</returns>
         public decimal GetPrice(string number, string message)
         {
             try
@@ -227,11 +367,12 @@ namespace Lib_SMS
                 request.ContentLength = data.Length;
                 Stream dataStream = request.GetRequestStream();
                 dataStream.Write(data, 0, data.Length);
+
                 using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
                     if (response.StatusCode != HttpStatusCode.OK)
                     {
-                        Console.WriteLine($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
+                        ShowErrorMessage($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
                         return -3;
                     }
 
@@ -242,7 +383,7 @@ namespace Lib_SMS
                     {
                         xmlReader.ReadToFollowing("status");
                         int status = xmlReader.ReadElementContentAsInt();
-                        if (status != 0)
+                        if (status < 0)
                             return status;
 
                         xmlReader.ReadToFollowing("amount");
@@ -253,7 +394,7 @@ namespace Lib_SMS
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                ShowErrorMessage(e.Message);
                 return -3;
             }
         }
@@ -264,7 +405,7 @@ namespace Lib_SMS
         /// <returns>>=0 — Баланс пользователя в гривнах
         /// -1 — Неправильный логин и/или пароль
         /// -2 — Неправильный формат XML
-        /// -3 — Ошибка подключения/исключение</returns>
+        /// -3 — Ошибка подключения</returns>
         public decimal GetBalance()
         {
             try
@@ -287,11 +428,12 @@ namespace Lib_SMS
                 request.ContentLength = data.Length;
                 Stream dataStream = request.GetRequestStream();
                 dataStream.Write(data, 0, data.Length);
+
                 using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
                 {
                     if (response.StatusCode != HttpStatusCode.OK)
                     {
-                        Console.WriteLine($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
+                        ShowErrorMessage($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
                         return -3;
                     }
 
@@ -302,7 +444,7 @@ namespace Lib_SMS
                     {
                         xmlReader.ReadToFollowing("status");
                         int status = xmlReader.ReadElementContentAsInt();
-                        if (status != 0)
+                        if (status < 0)
                             return status;
 
                         xmlReader.ReadToFollowing("amount");
@@ -313,9 +455,20 @@ namespace Lib_SMS
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                ShowErrorMessage(e.Message);
                 return -3;
             }
+        }
+
+        private void ShowErrorMessage(string message, ConsoleColor col = ConsoleColor.DarkCyan)
+        {
+            Console.Write(new string('—', 3) + ' ');
+            Console.ForegroundColor = col;
+            Console.Write("SMSSender");
+
+            Console.ResetColor();
+            Console.WriteLine($" ({DateTime.Now.ToString("HH':'mm':'ss")})");
+            Console.WriteLine($"    {message}");
         }
     }
 }
---
+++ Lib SMS/SMSState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_SMS
+{
+    public enum SMSState
+    {
+        SENT,                           // Отослано
+        NOT_DELIVERED,                  // Не доставлено
+        DELIVERED,                      // Доставлено
+        NOT_ALLOWED,                    // Оператор не обслуживается
+        INVALID_DESTINATION_ADDRESS,    // Неверный адрес для доставки
+        INVALID_SOURCE_ADDRESS,         // Неправильное имя «От кого»
+        NOT_ENOUGH_CREDITS,             // Недостаточно кредитов
+
+        ERROR                           // Произошла ошибка подключения
+    }
+}
